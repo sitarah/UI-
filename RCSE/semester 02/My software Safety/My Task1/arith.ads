5c9f78885250529c86fc3bad3bac723595a02de5
unknown <sitarah3126@gmail.com>
2024-07-23 00:21:53 +0200

    Remove extra files and directories

--- RCSE/semester 02/My software Safety/My Task1/arith.ads
+++
@@ -1,8 +0,0 @@
-package arith is
-   pragma Assertion_Policy (Check);
-   function Sqroot (Arg : Integer) return Integer
-     with Pre => Arg >= 0,
-          Post => (Sqroot'Result >= 0)
-                   and ((Sqroot'Result ** 2) <= Arg)
-                   and ((Sqroot'Result ** 2 + 1) > Arg);
-end arith;
